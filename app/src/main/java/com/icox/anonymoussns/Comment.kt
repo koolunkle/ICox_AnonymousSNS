69121f757f7a8397871aa77ee6f77b69f384f24c
koolunkle <gugi329@daum.net>
2022-01-22 21:29:03 +0900

    Post / Comment 클래스 구현

---
+++ app/src/main/java/com/icox/anonymoussns/Comment.kt
@@ -0,0 +1,10 @@
+package com.icox.anonymoussns
+
+class Comment {
+    var commentId = ""
+    var postId = ""
+    var writerId = ""
+    var message = ""
+    var writeTime: Any = ""
+    var backgroundUri = ""
+}
---
+++ app/src/main/java/com/icox/anonymoussns/Post.kt
@@ -0,0 +1,10 @@
+package com.icox.anonymoussns
+
+class Post {
+    var postId = ""
+    var writerId = ""
+    var message = ""
+    var writeTime: Any = Any()
+    var backgroundUri = ""
+    var commentCount = 0
+}
