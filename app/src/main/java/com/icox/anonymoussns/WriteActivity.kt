a699a2e830dd79bdc17fcc1495aec2d267e9a5fe
koolunkle <gugi329@daum.net>
2022-01-22 21:30:35 +0900

    게시글 작성 레이아웃 구현

---
+++ app/src/main/java/com/icox/anonymoussns/WriteActivity.kt
@@ -0,0 +1,11 @@
+package com.icox.anonymoussns
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write)
+    }
+}
